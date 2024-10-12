37b5ad0552ca5f1ba175f77144ba0f2c3beb9027
WhiteToggled <toggledwhite@gmail.com>
2024-10-13 01:16:26 +0500

    feat: implement data flow commands

--- src/InstructionSet.cpp
+++ src/InstructionSet.cpp
@@ -1,3 +1,21 @@
 #include "../include/InstructionSet.h"
 #include "../include/VoleMachine.h"
 
+void InstructionSet::LOAD(VoleMachine &machine, uint8_t reg, uint8_t address) {
+    uint8_t value = machine.read_memory(address);
+    machine.write_register(reg, value);
+}
+
+void InstructionSet::LOAD_VAL(VoleMachine &machine, uint8_t reg, uint8_t value) {
+    machine.write_register(reg, value);
+}
+
+void InstructionSet::STORE(VoleMachine &machine, uint8_t reg, uint8_t address) {
+    uint8_t value = machine.read_register(reg);
+    machine.write_memory(address, value);
+}
+    
+void InstructionSet::MOVE(VoleMachine &machine, uint8_t reg, uint8_t regDest) {
+    uint8_t value = machine.read_register(reg);
+    machine.write_register(regDest, value);
+}
