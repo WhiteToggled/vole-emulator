79973b411daa822f203bd5a54099996ef86c1de8
WhiteToggled <toggledwhite@gmail.com>
2024-10-12 22:02:28 +0500

    feat: implement decode_execute

--- src/VoleMachine.cpp
+++ src/VoleMachine.cpp
@@ -37,7 +37,7 @@ void VoleMachine::write_register(uint8_t reg, uint8_t value) {
     }
 }
 
-void VoleMachine::fetch(uint8_t address) {
+void VoleMachine::fetch() {
 
     uint8_t high_byte = read_memory(program_counter);
     uint8_t low_byte = read_memory(program_counter + 1);
@@ -48,3 +48,55 @@ void VoleMachine::fetch(uint8_t address) {
     instruction_register = instruction;
     program_counter += 2;
 }
+
+void VoleMachine::decode_execute() {
+
+    uint8_t opcode = instruction_register >> 12;      // _ X X X 
+    uint8_t reg = (instruction_register >> 8) & 0xF;  // X _ X X 
+    uint8_t regS = (instruction_register >> 4) & 0xF; // X X _ X
+    uint8_t regD = instruction_register & 0xF;        // X X X _
+    uint8_t valueXY = instruction_register & 0xFF;    // X X _ _
+
+    switch (opcode) {
+        case 0x1:
+            LOAD(*this, reg, valueXY);
+            break;
+        case 0x2:
+            LOAD_VAL(*this, reg, valueXY);
+            break;
+        case 0x3:
+            STORE(*this, reg, valueXY);
+            break;
+        case 0x4:
+            MOVE(*this, regS, regD);
+            break;
+        case 0x5:
+            ADD(*this, reg, regS, regD);
+            break;
+        case 0x6:
+            ADD_FLOAT(*this, reg, regS, regD);
+            break;
+        case 0x7:
+            OR(*this, reg, regS, regD);
+            break;
+        case 0x8:
+            AND(*this, reg, regS, regD);
+            break;
+        case 0x9:
+            XOR(*this, reg, regS, regD);
+            break;
+        case 0xA:
+            ROTATE(*this, reg, valueXY);
+            break;
+        case 0xB:
+            JUMP(*this, reg, valueXY);
+            break;
+        case 0xC:
+            HALT(*this);
+            break;
+        default: 
+            // idk Error
+            break;
+    }
+}
+
