d1b2592ba8810a337e5d8ee73ed2e9c3f7423452
WhiteToggled <toggledwhite@gmail.com>
2024-10-15 00:07:47 +0500

    test: basic debug frontend

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,53 @@
+#include <cstdint>
 #include <iostream>
+#include <sstream>
+#include <string>
+#include "../include/VoleMachine.h"
+#include "../include/InstructionSet.h"
 
-int main(int argc, char ** argv) {
+using namespace std;
+VoleMachine machine;
 
-    std::cout << "Hello" << std::endl;
+void input_memory() {
+
+    string hex_str;
+    unsigned hex_value;
+    int i = 0;
+    do {
+
+        cout << "Please enter the address for memory cell " << i << ": (or q to quit) " << endl;
+        cin >> hex_str;
+
+        if (hex_str == "q" || hex_str == "Q") 
+            return;
+
+        stringstream ss;
+
+        ss << hex << hex_str;
+        ss >> hex_value;
+
+        machine.write_memory(i, static_cast<uint8_t>(hex_value));
+
+
+        i++;
+    } while (i < 256);
+
+}
+
+int main(int argc, char* argv[]) {
+
+
+    input_memory();
+    cout << "------------------" << endl;
+    cout << "     Initial      " << endl;
+    cout << "------------------" << endl;
+    machine.get_current_state();
+    machine.start_machine();
+    cout << "------------------" << endl;
+    cout << "       Final      " << endl;
+    cout << "------------------" << endl;
+    machine.get_current_state();
 
     return 0;
+
 }
