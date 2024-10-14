a5369ed2ba97731ce4b376d65af7375cc5866bf1
WhiteToggled <toggledwhite@gmail.com>
2024-10-14 23:22:34 +0500

    feat: define debug methods

--- include/VoleMachine.h
+++ include/VoleMachine.h
@@ -17,17 +17,27 @@ public:
     uint8_t read_register(uint8_t reg) const;
     void write_register(uint8_t reg, uint8_t value);
 
-    uint8_t get_program_counter() const;
     void set_program_counter(uint8_t address);
 
     void fetch();
     void decode_execute();
+
+    void start_machine();
+    void halt_machine();
+
+    // Debugging
+    void get_current_state() const;
+    void get_program_counter() const;
+    void get_instruction_register() const;
+    void get_memory() const;
+    void get_registers() const;
 
 private:
     uint8_t memory[MEMORY_SIZE];
     uint8_t registers[NUM_REGISTERS];
     uint8_t program_counter;
     uint16_t instruction_register;
+    bool is_halted;
 };
 
 #endif // !VOLEMACHINE_H
